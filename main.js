e2abe240290cea2f221df228a3a7dc8f82477f29
Vandana Sivadasan <51973070+Vandana-Sivadasan@users.noreply.github.com>
2022-02-14 19:39:07 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+noseX=0;
+noseY=0;
+diffrence=0;
+rightWristX=0;
+leftWristX=0;
+
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log('poseNet is initialized');
+
+}
+function gotPoses(results){
+  if(results.length>0){
+      console.log(results)
+  }
+}
+function draw(){
+    background('rgb(0,255,0)');
+    
+}
